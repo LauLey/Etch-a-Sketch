f5695c93a39357a2b5c59de9a6aa1a12f5f8d624
LauLey <226139236+LauLey@users.noreply.github.com>
2026-06-26 15:10:20 -0600

    Crea la cuadricula dinamica y agrega la logica inicial para personalizar su tamaño

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
 const reiniciar = document.querySelector("#reinicio");
 const personaliza = document.querySelector("#personaliza");
 const principalContainer = document.querySelector(".principal_container");
-const div = document.createElement("div");
-//aun no se si mis estilos principales se escriben asi y si deberian estar dentro de la
-//funcion principalCuadricula
-div.style.backgroundColor = "white";
-div.style.width = "100%";
-div.style.border = "solid 2px red";
-principalContainer.appendChild(div);
-
-function principalCuadricula {
-    principalContainer.appendChild(div);
-    //hacerlo 16*16 veces con un bucle o copiar y pegar manualmente//
-    //reiniciar estilos//
+const widthContainer = 960;
+function createCuadricula (num) {
+    const columns = Math.sqrt(num);
+    const size = widthContainer / columns;
+    for ( let i = 0; i < num; i++) {
+        const div = document.createElement("div");
+        div.style.backgroundColor = "white";
+        div.style.border = "solid 2px black";
+        div.style.margin = "0";
+        div.style.height = size + "px";
+        div.style.width = size + "px";
+        div.style.boxSizing = "border-box";
+        principalContainer.appendChild(div);
+    }
 }
 
-function createCuadricula (numDivs) {
-principalContainer.appendChild(div); //por el numDivs mediante bucle
+function removeCuadricula () {
+//escribir funcion para borrar todos los divs
+// existentes dentro del principal Container por si hacen click varias veces en el boton personaliza
 }
 
-reiniciar.addEventListener("click" () => {
-    principalCuadricula() //para mostrar en la pantalla limpia desde cero cada vez que hagan click en reinicio y de 16 x 16//
+reiniciar.addEventListener("click", () => {
+    createCuadricula (256);
 })
 
-personaliza.addEventListener("click" () => {
+personaliza.addEventListener("click", () => {
+    removeCuadricula();
     let numero = prompt ("Introduce numero de columnas del 1 al 100 (solo enteros):");
     if ((numero>100)||(numero<1)){
-        alert(Introduce un número del 1 al 100)
+        alert("Introduce un número del 1 al 100");
+        }
+    else {
+        let numDivs = numero * numero;
+        createCuadricula(numDivs);
     }
-    let numDivs = numero * numero;
-    createCuadricula(numDivs);
-})
-
-div.addEventListener("mousemove" () => {
-    //cambiar el color cuando sienta que el mouse pasa una vez e 
-    // ir oscureciendo gradualmente cada vez que pasa y a la decima vez oscurecer totalmente el div//
-})
-
-principalCuadricula()//segun yo ejecutar esta funcion me mostrara desde el inicio la cuadricula con 16 x 16 divs
+    })
